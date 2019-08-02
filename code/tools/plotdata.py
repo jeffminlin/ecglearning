eacf7449dd5c09f716c30aa05a59ed5b3cd0e2d3
jeffminlin <jeffmin.lin@gmail.com>
2019-08-02 11:53:09 -0700

    confusion matrices, plotting losses, logging

--- code/tools/plotdata.py
+++ code/tools/plotdata.py
@@ -6,17 +6,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_processed_data(df, title, xlabel, ylabel, process):
+def plot_processed_data(df, title, xlabel, ylabel, process, num):
     """Make a figure with subplots after processing the data."""
 
     num = len(df.index)
-    fig, axes = plt.subplots(num, 1, sharex=True, figsize=(8,8))
+    fig, axes = plt.subplots(num, 1, sharex=True, figsize=(8, 2.5*num))
     fig.suptitle(title)
     for idx in range(num):
         axes[idx].title.set_text("Sample number " + str(df.index[idx]))
         axes[idx].set_ylabel(ylabel)
         axes[idx].plot(
-            process(df.iloc[idx]), label="Class: " + str(int(df.iloc[idx, -1]))
+            *process(df.iloc[idx]), label="Class: " + str(int(df.iloc[idx, -1]))
         )
         axes[idx].legend()
     # Shared xlabel
@@ -26,27 +26,36 @@ def plot_processed_data(df, title, xlabel, ylabel, process):
     return fig
 
 
-def plot_ecg(filename, num):
+def plot_ecg(filename, samplerate, num):
     """Assumes that the last column is the class."""
 
     df = pd.read_csv(filename, header=None)
-    # df.rename(columns={df.columns[-1]: "Classes"}, inplace=True)
-    # print("Count of each class")
-    # print(df.groupby("Classes").size())
-    data = df.sample(n=num, axis=0)
+    df.rename(columns={df.columns[-1]: "Classes"}, inplace=True)
+
+    data = df.groupby("Classes", group_keys=False).apply(
+        pd.DataFrame.sample, n=num, axis=0
+    )
     fig = plot_processed_data(
         data,
-        "Random " + str(num) + " heartbeats in " + filename,
+        "Random samples of each type of heartbeat in " + filename,
+        "seconds",
         "",
-        "",
-        lambda row: np.trim_zeros(row[:-1]),
+        lambda row: (
+            np.arange(len(np.trim_zeros(row[:-1]))) / samplerate,
+            np.trim_zeros(row[:-1]),
+        ),
+        len(data.index)
     )
     fig_freq = plot_processed_data(
         data,
-        "Magnitude response of random " + str(num) + " heartbeats",
-        "Frequency",
+        "Magnitude responses of each heartbeat",
+        "Hz",
         "",
-        lambda row: np.abs(np.fft.rfft(np.trim_zeros(row[:-1]))),
+        lambda row: (
+            np.fft.rfftfreq(len(np.trim_zeros(row[:-1])), d=(1.0 / samplerate)),
+            np.abs(np.fft.rfft(np.trim_zeros(row[:-1]))),
+        ),
+        len(data.index)
     )
 
     plt.show()
--- code/tools/train.py
+++ code/tools/train.py
@@ -3,8 +3,11 @@ import sys
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from tensorflow.keras import callbacks
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
 
 from . import models as mod
 
@@ -15,6 +18,7 @@ def preprocess(trainfile, testfile, fft=False):
     datasets = {}
     dataset_arrays = {}
     labels = {}
+    sparse_labels = {}
     for key, name in filenames.items():
         # Read in dataset
         datasets[key] = pd.read_csv(name, header=None)
@@ -30,6 +34,7 @@ def preprocess(trainfile, testfile, fft=False):
             datasets[key]["Classes"],
             num_classes=len(datasets[key].groupby("Classes").size()),
         )
+        sparse_labels[key] = datasets[key]["Classes"].to_numpy()
 
         # Isolate data and convert to numpy array
         dataset_arrays[key] = datasets[key].drop("Classes", axis=1)
@@ -38,7 +43,7 @@ def preprocess(trainfile, testfile, fft=False):
     if fft:
         dataset_arrays = dataset_fft(dataset_arrays)
 
-    return dataset_arrays, labels, datasets
+    return dataset_arrays, labels, sparse_labels, datasets
 
 
 def class_count(datasets):
@@ -59,9 +64,9 @@ def dataset_fft(dataset_arrays):
     return datasets_fft
 
 
-def train_dense(datasets, labels, hiddenlayers, config):
+def train_dense(dataset_arrays, labels, hiddenlayers, config):
 
-    inputsize = datasets["train"].shape[1]
+    inputsize = dataset_arrays["train"].shape[1]
     ncategories = labels["train"].shape[1]
     model = mod.create_dense(inputsize, hiddenlayers, ncategories)
     model.summary()
@@ -69,21 +74,103 @@ def train_dense(datasets, labels, hiddenlayers, config):
     print()
     print("Training")
     earlystop = callbacks.EarlyStopping(
-        monitor="val_loss", patience=3, restore_best_weights=True
+        monitor="val_loss",
+        patience=config["patience"],
+        restore_best_weights=True,
+        min_delta=1.0e-4,
     )
+    tensorboard_callback = callbacks.TensorBoard(log_dir=config["logdir"])
     model.compile(
-        optimizer="Nadam",
-        loss="categorical_crossentropy",
+        optimizer=config["optimizer"],
+        loss=config["loss"],
         metrics=[tf.keras.metrics.categorical_accuracy],
     )
-    model.fit(
-        datasets["train"],
+    history = model.fit(
+        dataset_arrays["train"],
         labels["train"],
+        verbose=config["verbose"],
         batch_size=config["batch_size"],
         epochs=config["epochs"],
-        callbacks=[earlystop],
+        callbacks=[earlystop, tensorboard_callback],
         validation_split=config["val_split"],
     )
-    print()
-    print("Testing")
-    model.evaluate(datasets["test"], labels["test"])
+    print(
+        "Train acc:",
+        model.evaluate(dataset_arrays["train"], labels["train"], verbose=0)[1],
+    )
+    print(
+        "Test acc:",
+        model.evaluate(dataset_arrays["test"], labels["test"], verbose=0)[1],
+    )
+
+    return history, model
+
+
+def plot_fit_history(history):
+
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.yscale("log")
+    plt.plot(history.history["loss"], ".-", label="loss")
+    try:
+        plt.plot(history.history["val_loss"], ".-", label="val_loss")
+    except KeyError:
+        print("No validation loss")
+    plt.legend()
+    plt.show()
+
+
+def plot_cm(y_true, y_pred, classes, normalize=False, title=None, cmap=plt.cm.Blues):
+    """
+    This function from the scikit-learn website prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = "Normalized confusion matrix"
+        else:
+            title = "Confusion matrix, without normalization"
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred, labels=classes)
+    if normalize:
+        cm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+    #     print("Normalized confusion matrix")
+    # else:
+    #     print("Confusion matrix, without normalization")
+
+    # print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation="nearest", cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(
+        xticks=np.arange(cm.shape[1]),
+        yticks=np.arange(cm.shape[0]),
+        # ... and label them with the respective list entries
+        xticklabels=classes,
+        yticklabels=classes,
+        title=title,
+        ylabel="True label",
+        xlabel="Predicted label",
+    )
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = ".2f" if normalize else "d"
+    thresh = cm.max() / 2.0
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(
+                j,
+                i,
+                format(cm[i, j], fmt),
+                ha="center",
+                va="center",
+                color="white" if cm[i, j] > thresh else "black",
+            )
+    fig.tight_layout()
+    plt.show()
