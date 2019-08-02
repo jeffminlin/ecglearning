5a3aac621710ecb11fcf8e61cf50c0464c107f1d
jeffminlin <jeffmin.lin@gmail.com>
2019-08-02 12:26:57 -0700

    swapped to softmax

--- code/tools/models.py
+++ code/tools/models.py
@@ -10,6 +10,6 @@ def create_dense(inputsize, hiddenlayers, ncategories):
     outputs = layers.Dense(hiddenlayers[0][0], activation=hiddenlayers[0][1])(inputs)
     for layer in hiddenlayers[1:]:
         outputs = layers.Dense(layer[0], activation=layer[1])(outputs)
-    outputs = layers.Dense(ncategories, activation="sigmoid")(outputs)
+    outputs = layers.Dense(ncategories, activation="softmax")(outputs)
 
     return tf.keras.Model(inputs=inputs, outputs=outputs, name="dense_model")
